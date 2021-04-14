ce169cba1f5ce21a035b2aab901bf06a90c03e10
Sonny <libertines11@gmail.com>
2021-04-15 08:12:54 +0900

    's'

--- screens/Movies/MoviePresenter.js
+++ screens/Movies/MoviePresenter.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-web-swiper';
-import { Dimensions, ActivityIndicator } from 'react-native';
+import { Dimensions, ActivityIndicator, View, Dimensions } from 'react-native';
 import Slide from '../../components/Movies/Slide';
 
-const {width, height} = Dimensions.get("screen");
+const {width:WIDTH, height:HEIGHT} = Dimensions.get("screen");
 
 // 아래의 ``은 태그드탬플릿으로 아래의 링크를 참고하자
 // https://velog.io/@lesh/%ED%85%9C%ED%94%8C%EB%A6%BF-%EB%A6%AC%ED%84%B0%EB%9F%B4Template-Literal%EA%B3%BC-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%93%9C-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8styled-component
@@ -13,6 +13,11 @@ const Container = styled.View`
   flex: 1;
   background-color: black;
   justify-content: center;
+`;
+
+const SliderContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${HEIGHT / 4}px;
 `;
 
 // const Header = styled.View`
@@ -37,20 +42,20 @@ const MoviePresenter = ({loading, nowPlaying}) => {
       {loading ? (
         <ActivityIndicator color="white" size="small" />
       ) : (
-        <>
+        <SliderContainer>
           <Swiper controlsEnabled={false} loop timeout={3} >
             {nowPlaying.map((movie) => (
               <Slide 
                 key={movie.id}
                 id={movie.id}
-                title={movie.original_language}
+                title={movie.original_title}
                 overview={movie.overview}
                 votes={movie.vote_average}
                 backgroundImage={movie.backdrop_path}
               />
             ))}
           </Swiper>
-        </>
+        </SliderContainer>
       )}
     </Container>
   );
