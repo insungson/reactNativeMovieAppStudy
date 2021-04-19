20947f08e7e3ff3211496266e824a27ee2b8d1f4
Sonny <libertines11@gmail.com>
2021-04-20 08:16:14 +0900

    's'

---
+++ components/Search/Input.js
@@ -0,0 +1,7 @@
+//시뮬레이터에서 Hardware > KeyBoard > Toggle Software Keyboard 에 체크를 표시해주면 앱에서 키보드모양이 생긴다.
+//https://reactnative.dev/docs/textinput 에서 보면 TextInput 에서  returnKeyType은 키보드에서 엔터속성을 속성의 value로 바꿔준다.
+// autoCorrect 옵션을 false 로 해주면 단어 자동완성 예시도 없애준다.
+// autoCompleteType 옵션은 자동으로 완성시켜주는 타입을 고를 수도 있다.
+// keyboardType 옵션은 앱의 화면에 보여주는 키보드의 타입을 고를 수 있다. ** 이건 좀 쓰일듯..
+// onChangeText 는 React의 onChange와 같은 기능을 해준다
+// onSubmitEditing 은 React의 onSubmit과 같은 기능을 해준다.
