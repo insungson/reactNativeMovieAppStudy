76c12a0ea16a90468496eb43ac86500398041156
Sonny <sis_0101@coresight.io>
2021-04-20 18:33:06 +0900

    숙제 추가

--- components/List.js
+++ components/List.js
@@ -1,14 +1,14 @@
 //여기서 만든것은.. HorizontalSlider(가로스크롤)와 다르게  세로로 스크롤되는 것이다.
-// List 와 같은 컴포넌트는 reuseable component 를 만든것이다. 
-import React from 'react';
-import styled from 'styled-components';
-import Title from './Title';
+// List 와 같은 컴포넌트는 reuseable component 를 만든것이다.
+import React from "react";
+import styled from "styled-components/native";
+import Title from "./Title";
 
 const Container = styled.View`
   margin-top: 20px;
 `;
 
-const List = ({title, children}) => {
+const List = ({ title, children }) => {
   return (
     <>
       <Title title={title} />
--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ScrollContainer from '../../components/ScrollContainer';
-import HorizontalSlider from '../../components/HorizontalSilder';
-import Vertical from '../../components/Vertical';
-import Horizontal from '../../components/Horizontal';
-import styled from 'styled-components/native';
-import List from '../../components/List';
+import React from "react";
+import ScrollContainer from "../../components/ScrollContainer";
+import HorizontalSlider from "../../components/HorizontalSilder";
+import Vertical from "../../components/Vertical";
+import Horizontal from "../../components/Horizontal";
+import styled from "styled-components/native";
+import List from "../../components/List";
 
-// <></> 이렇게 fragment 안에 있는 HorizontalSlider 를 render 할 수 없다. 
+// <></> 이렇게 fragment 안에 있는 HorizontalSlider 를 render 할 수 없다.
 // 왜냐하면.. HorizontalSlider 안에도 <></> 처럼 다른 Fragment 가 있기 때문에...
 // 안드로이드는 되고.. 웹에선 동작이 안되었다.. 이부분을 바꾸기 위해 <> 대신에 View태그를 만들어주면 된다.
 
@@ -14,13 +14,13 @@ const Container = styled.View`
   margin-top: 30px;
 `;
 
-const TvPresenter = ({loading, popular, topRated, today}) => {
+const TvPresenter = ({ loading, popular, topRated, today, thisWeek }) => {
   return (
     <ScrollContainer loading={loading}>
       <Container>
         <HorizontalSlider title="Popular Shows">
-          {popular.map(show => (
-            <Vertical 
+          {popular.map((show) => (
+            <Vertical
               id={show.id}
               key={show.id}
               poster={show.poster_path}
@@ -30,8 +30,8 @@ const TvPresenter = ({loading, popular, topRated, today}) => {
           ))}
         </HorizontalSlider>
         <HorizontalSlider title="Top Rated">
-          {topRated.map(show => (
-            <Vertical 
+          {topRated.map((show) => (
+            <Vertical
               id={show.id}
               key={show.id}
               poster={show.poster_path}
@@ -40,9 +40,20 @@ const TvPresenter = ({loading, popular, topRated, today}) => {
             />
           ))}
         </HorizontalSlider>
+        <HorizontalSlider title={"On The Air This Week!"}>
+          {thisWeek.map((show) => (
+            <Vertical
+              id={show.id}
+              key={show.key}
+              poster={show.poster_path}
+              title={show.original_name}
+              votes={show.vote_average}
+            />
+          ))}
+        </HorizontalSlider>
         <List title="Airing Today">
-          {today.map(show => (
-            <Horizontal 
+          {today.map((show) => (
+            <Horizontal
               key={show.id}
               id={show.id}
               title={show.name}
